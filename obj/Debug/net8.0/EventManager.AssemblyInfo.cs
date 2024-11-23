dc50a96b928cca8579a5870bb88972a4d4196217
Halbert Nascimento <halbert.fac@gmail.com>
2024-11-23 09:36:26 -0300

    template login

--- obj/Debug/net8.0/EventManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1a453549008088fcb45164fa933d5a0931a22b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def018c58052333587b4a130f1586b684c639500")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
