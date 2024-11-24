e79bba071b7f344ee510b5da17d371a0605341b5
Halbert Nascimento <halbert.fac@gmail.com>
2024-11-24 14:36:19 -0300

    template pagina de confirmaçã ode delete alterado o layout

--- obj/Debug/net8.0/EventManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1ebdd2eb6c60fdcffba2123e116fca7785618c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a170a05bcdec26290b2e953bbd964ff7e9bc4d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
