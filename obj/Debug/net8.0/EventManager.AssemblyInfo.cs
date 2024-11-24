4f91dd28036262f0cf3e2f89abf24a21cccf6336
Halbert Nascimento <halbert.fac@gmail.com>
2024-11-24 17:13:56 -0300

    template e mensagem informatica sobre exclusão de  perfil

--- obj/Debug/net8.0/EventManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215f812e91e3112183ed3fb20ba855cccc18cea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9d261ba5a972e817e8660454650d5215a960bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
