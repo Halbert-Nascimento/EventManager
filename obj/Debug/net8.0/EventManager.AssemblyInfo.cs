e7dce8889fccf9d5697a6128783997abd27f3166
Halbert Nascimento <halbert.fac@gmail.com>
2024-11-27 17:16:11 -0300

    atualização de templates

--- obj/Debug/net8.0/EventManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+167c46418022ca983e87548a2799878a70331181")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8241e3a27984029038972507da6d2090a61d767")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
